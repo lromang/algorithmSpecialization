b87cb096f593c1ba1975008d408577791fc2a573
lromang <luis.roangarci@gmail.com>
2017-01-30 20:28:31 -0600

    first programming assignment

---
+++ algorithmic-toolbox/week1/aplusb.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  /* Array initialization. */
+  int a, b;
+  /* Reading input from standard input. */
+  scanf("%d %d", &a, &b);
+  /* Outputing sum. */
+  printf("%d", a + b);
+}
