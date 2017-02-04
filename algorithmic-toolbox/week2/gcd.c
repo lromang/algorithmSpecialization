fdb8dc37b599ac569f22c6c5cf2b0d8baf1caefb
lromang <luis.roangarci@gmail.com>
2017-02-03 21:37:12 -0600

    gcd

--- algorithmic-toolbox/week2/gcd.c
+++ algorithmic-toolbox/week2/gcd.c
@@ -3,11 +3,12 @@
 
 int naiveGcd(int, int);
 int eucGcd(int, int);
+int eucGcd2(int, int);
 
 int main(){
   int a, b;
   scanf("%d %d", &a, &b);
-  printf("%d", eucGcd(a, b));
+  printf("%d", eucGcd2(a, b));
   return 0;
 }
 
@@ -17,7 +18,7 @@ int naiveGcd(int a, int b){
   int gcd, k, min;
   /* Varible initialization */
   k   = 1;
-  gcd = 0;
+  gcd = a == 0 ? b : b == 0 ? 0 : b;
   min = a < b ? a: b;
   /* Test all posible divisors */
   while(k <= min){
@@ -27,14 +28,16 @@ int naiveGcd(int a, int b){
   return gcd;
 }
 
-
 int eucGcd(int a, int b){
   int min, max;
   min = a < b ? a: b;
   max = a > b ? a: b;
-  if( max % min == 0 ){
-    return min;
-  }else{
-    return eucGcd(max, max % min);
-  }
+  if( min == 0 ) return max;
+  if( max % min == 0 ) return min;
+  return eucGcd(max, max % min);
+}
+
+int eucGcd2(int a, int b){
+  if(b == 0) return a;
+  return eucGcd2(b, a % b);
 }
