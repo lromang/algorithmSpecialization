c9851a4025e188e7c17febc14fc439727eb6f36b
lromang <luis.roangarci@gmail.com>
2017-02-06 22:09:50 -0600

    last digit fib

--- algorithmic-toolbox/week2/fibonacci.c
+++ algorithmic-toolbox/week2/fibonacci.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 int naiveFib(int);
+int lastDFib(int);
 
 int main(){
   int n;
   /* Read in n for fibonacci progression */
   scanf("%d", &n);
   /* Print results */
-  printf("%d", naiveFib(n));
+  printf("%d", lastDFib(n));
   /* Return */
   return 0;
 }
@@ -20,9 +21,24 @@ int naiveFib(int n){
   /* Memory allocation */
   fib = (long long*) malloc((n + 1) * sizeof(long long));
   /* Base case */
-  fib[1] = fib[0] = 1;
+  fib[0] = 0;
+  fib[1] = 1;
   for(i = 2; i < (n + 1); i++){
     fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[n];
+}
+
+int lastDFib(int n){
+  int i;
+  long long *fib;
+  /* Memory allocation */
+  fib = (long long*) malloc((n + 1) * sizeof(long long));
+  /* Base case */
+  fib[0] = 0;
+  fib[1] = 1;
+  for(i = 2; i < (n + 1); i++){
+    fib[i] = (fib[i - 1] + fib[i - 2]) % 10;
   }
   return fib[n];
 }
