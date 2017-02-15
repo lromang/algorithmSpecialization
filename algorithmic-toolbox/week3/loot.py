107ece29bbff46dacd39b294d3de5c0a36382d40
lromang <luis.roangarci@gmail.com>
2017-02-15 14:16:44 -0600

    loot

--- algorithmic-toolbox/week3/loot.py
+++ algorithmic-toolbox/week3/loot.py
@@ -1,15 +1,34 @@
 #python2
+
 import sys
+
+## knapsack
+def knapsack(v, w, c):
+    """
+    v = value of items.
+    w = weight of itmes.
+    c = capacity of knapsack.
+    """
+    tot          = 0
+    fracs        = [float(n)/float(d) for n, d in zip(v, w)]
+    sortedIndexs = sorted(range(len(fracs)), key = lambda k: fracs[k], reverse = True)
+    i = 0
+    while(c > 0 and i < len(v)):
+        frac_in  = min(1, float(c)/float(w[sortedIndexs[i]]))
+        c        = c   - frac_in*w[sortedIndexs[i]]
+        tot      = tot + frac_in*v[sortedIndexs[i]]
+        i        = i + 1
+    return float(tot)
+
 
 ## Running variables.
 n        = sys.stdin.readline()
 weights  = []
 values   = []
-for i in range(int(n[0])):
+c        = int(n.split(' ')[1])
+for i in range(int(n.split(' ')[0])):
     item = sys.stdin.readline().rstrip('\n')
     item = item.split(' ')
     values.append(int(item[0]))
     weights.append(int(item[1]))
-
-print values
-print weights
+print '%f' % knapsack(values, weights, c)
