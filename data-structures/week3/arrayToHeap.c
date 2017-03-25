51fc1d5c2ab9f9684f1d872aca6bc8d9b0fcb2e9
lromang <luis.roangarci@gmail.com>
2017-03-24 21:31:33 -0600

    not working

--- data-structures/week3/arrayToHeap.c
+++ data-structures/week3/arrayToHeap.c
@@ -5,8 +5,8 @@
 void swiftUp(int*, int);
 void buildHeap(int*, int**, int);
 void swap(int*, int, int);
-int leftChild(int*, int);
-int rightChild(int*, int);
+int leftChild(int);
+int rightChild(int);
 int parent(int*, int);
 int min(int*, int, int, int, int);
 void printPrintable(int**);
@@ -38,12 +38,12 @@ int parent(int* array, int index){
   return array[(int)floor((index - 1) / 2)];
 }
 
-int leftChild(int* array, int index){
-  return array[index * 2 + 1];
+int leftChild(int index){
+  return index * 2 + 1;
 }
 
-int rightChild(int* array, int index){
-  return array[(index + 1) * 2];
+int rightChild(int index){
+  return (index + 1) * 2;
 }
 
 void swap(int* array, int index1, int index2){
@@ -56,32 +56,40 @@ void swap(int* array, int index1, int index2){
 int min(int* array, int indexP, int indexL, int indexR, int size){
   int minIndex;
   minIndex = indexP;
-  if((indexL < size) && (array[indexP] < array[indexL]))
+  if((indexL < size) && (array[indexP]   > array[indexL]))
     minIndex = indexL;
-  if((indexR < size) && (array[indexR] < array[minIndex]))
+  if((indexR < size) && (array[minIndex] > array[indexR]))
     minIndex = indexR;
   return minIndex;
 }
 
 void buildHeap(int* array, int** pResults, int size){
   int i, minIndex, j, searchSize, k;
-  searchSize = (int)floor((size - 1) / 2) - 1;
+  searchSize = (int)floor((size - 2) / 2);
   k = 0;
   for(i = searchSize; i >= 0; i--){
-    for(j = i; j < size; j++){
+    // printf("i = %d\n", i);
+    j = i;
+    while(j <= (int)floor(size / 2)){
+      /*
+       *printf("j = %d\n", j);
+       *printf("left Child = %d \n", leftChild(j));
+       *printf("right Child = %d \n", rightChild(j));
+      */
       minIndex = min(array,
                      j,
-                     leftChild(array, j),
-                     rightChild(array, j),
+                     leftChild(j),
+                     rightChild(j),
                      size);
       if(minIndex == j)
         break;
       swap(array, j, minIndex);
-      j = minIndex;
       // Printing variables
-      pResults[0][0]++;
       pResults[1][k]     = j;
       pResults[1][k + 1] = minIndex;
+      // Update
+      j = minIndex;
+      pResults[0][0]++;
       k+=2;
     }
   }
@@ -92,8 +100,8 @@ void printPrintable(int** pResults){
   printf("%d\n", pResults[0][0]);
   for(k = i = 0; i < pResults[0][0]; i++){
     printf("%d %d\n",
-           pResults[1][k % 2],
-           pResults[1][(k + 1) % 2]);
-    k++;
+           pResults[1][k],
+           pResults[1][(k + 1)]);
+    k+=2;
   }
 }
