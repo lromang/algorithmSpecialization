233c3b8c3e778ebab140ee7e3102ab12f21fac8d
lromang <luis.roangarci@gmail.com>
2017-03-24 20:49:21 -0600

    not working

--- data-structures/week3/arrayToHeap.c
+++ data-structures/week3/arrayToHeap.c
@@ -3,30 +3,33 @@
 #include <math.h>
 
 void swiftUp(int*, int);
-void buildHeap(int*, int);
+void buildHeap(int*, int**, int);
 void swap(int*, int, int);
 int leftChild(int*, int);
 int rightChild(int*, int);
 int parent(int*, int);
 int min(int*, int, int, int, int);
+void printPrintable(int**);
 
 int main(){
+  int** printables;
   int* array;
   int n, i;
   // Scan size of array.
   scanf("%d", &n);
   // Allocate memory for array.
-  array = (int*)malloc(n*sizeof(n));
+  array         = (int*)calloc(n, sizeof(int));
+  printables    = (int**)calloc((n + 1), sizeof(int));
+  printables[0] = (int*)calloc(1, sizeof(int));
+  printables[1] = (int*)calloc(n, sizeof(int));
   // Scan elements of array.
   for(i = 0; i < n; i++){
     scanf("%d", &array[i]);
   }
   // Build Heap.
-  buildHeap(array, n);
+  buildHeap(array, printables,  n);
   // Print elements of array.
-  for(i = 0; i < n; i++){
-    printf("%d ", array[i]);
-  }
+  printPrintable(printables);
   return 0;
 }
 
@@ -60,13 +63,12 @@ int min(int* array, int indexP, int indexL, int indexR, int size){
   return minIndex;
 }
 
-void buildHeap(int* array, int size){
-  int i, minIndex, j, searchSize;
+void buildHeap(int* array, int** pResults, int size){
+  int i, minIndex, j, searchSize, k;
   searchSize = (int)floor((size - 1) / 2) - 1;
+  k = 0;
   for(i = searchSize; i >= 0; i--){
-    printf("i = %d \n", i);
     for(j = i; j < size; j++){
-    printf("j = %d \n", j);
       minIndex = min(array,
                      j,
                      leftChild(array, j),
@@ -76,6 +78,22 @@ void buildHeap(int* array, int size){
         break;
       swap(array, j, minIndex);
       j = minIndex;
+      // Printing variables
+      pResults[0][0]++;
+      pResults[1][k]     = j;
+      pResults[1][k + 1] = minIndex;
+      k+=2;
     }
+  }
+}
+
+void printPrintable(int** pResults){
+  int i, k;
+  printf("%d\n", pResults[0][0]);
+  for(k = i = 0; i < pResults[0][0]; i++){
+    printf("%d %d\n",
+           pResults[1][k % 2],
+           pResults[1][(k + 1) % 2]);
+    k++;
   }
 }
